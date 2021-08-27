9fd2883e0a1d6840fd7bce7c962a8d0838fee1b2
Luqman <luqdek@gmail.com>
2021-08-27 15:36:30 +0100

    set frame to be resizeable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setTitle("Testing GUI");
+        frame.setResizable(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
         frame.setVisible(true);
