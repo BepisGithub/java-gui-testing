4039d498a000a33027b2cdd91a2411d4f9846982
Luqman <luqdek@gmail.com>
2021-08-27 15:33:19 +0100

    add title for program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        frame.setTitle("Testing GUI");
         frame.setSize(500, 500);
         frame.setVisible(true);
     }
