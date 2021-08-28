e22c2459cf3e415f14c11497192775a8d5ce9549
Luqman <luqdek@gmail.com>
2021-08-28 08:22:42 +0100

    add code for myframe class

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -1,6 +1,14 @@
 package com.company;
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
-
+    public MyFrame(){
+        this.setTitle("Testing GUI");
+        this.setResizable(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setVisible(true);
+        this.getContentPane().setBackground(Color.red);
+    }
 }
