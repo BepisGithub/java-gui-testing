932bf4fee4c2c8f12890c95a4053bf412365d252
Luqman <luqdek@gmail.com>
2021-08-28 12:12:54 +0100

    add labels to frame to see how the layout effects it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,7 +23,14 @@ public class Main {
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
-        JLabel basic = new JLabel("Sample text");
-        frame.add(basic);
+        JLabel basic1 = new JLabel("Sample text");
+        JLabel basic2 = new JLabel("Sample text");
+        JLabel basic3 = new JLabel("Sample text");
+        JLabel basic4 = new JLabel("Sample text");
+
+        frame.add(basic1);
+        frame.add(basic2);
+        frame.add(basic3);
+        frame.add(basic4);
     }
 }
