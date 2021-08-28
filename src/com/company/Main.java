8d56cc07cf32a8a68ee5630390a23ceaee033926
Luqman <luqdek@gmail.com>
2021-08-28 11:57:22 +0100

    comment out code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,17 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        /* JFrame frame = new JFrame();
-        frame.setTitle("Testing GUI");
-        frame.setResizable(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(Color.red); */
-        MyFrame frame = new MyFrame();
-
-        JLabel label = new JLabel();
-        label.setText("Sample text");
-        frame.add(label);
+//        /* JFrame frame = new JFrame();
+//        frame.setTitle("Testing GUI");
+//        frame.setResizable(true);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(500, 500);
+//        frame.setVisible(true);
+//        frame.getContentPane().setBackground(Color.red); */
+//        MyFrame frame = new MyFrame();
+//
+//        JLabel label = new JLabel();
+//        label.setText("Sample text");
+//        frame.add(label);
     }
 }
