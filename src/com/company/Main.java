657696676767cebd639b1f388202e594af6173a1
Luqman <luqdek@gmail.com>
2021-08-29 10:38:18 +0100

    create basic jframe instance

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
     }
 }
