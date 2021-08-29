b30be271da4970c8b8e8d96f77eab9abc871933b
Luqman <luqdek@gmail.com>
2021-08-29 10:37:04 +0100

    remove code for neatness

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,32 +5,6 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-//        /* JFrame frame = new JFrame();
-//        frame.setTitle("Testing GUI");
-//        frame.setResizable(true);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(500, 500);
-//        frame.setVisible(true);
-//        frame.getContentPane().setBackground(Color.red); */
-//        MyFrame frame = new MyFrame();
-//
-//        JLabel label = new JLabel();
-//        label.setText("Sample text");
-//        frame.add(label);
-
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
-        frame.setSize(500, 500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new FlowLayout());
-        JLabel basic1 = new JLabel("Sample text");
-        JLabel basic2 = new JLabel("Sample text");
-        JLabel basic3 = new JLabel("Sample text");
-        JLabel basic4 = new JLabel("Sample text");
-
-        frame.add(basic1);
-        frame.add(basic2);
-        frame.add(basic3);
-        frame.add(basic4);
+        
     }
 }
