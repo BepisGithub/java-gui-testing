07bef30d725b29bc7e92555e4725a1ebb1d6f3a5
Luqman <luqdek@gmail.com>
2021-08-30 14:32:22 +0100

    remove code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,22 +6,6 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        Border border = BorderFactory.createLineBorder(Color.GREEN, 3);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(500,500);
-        frame.setVisible(true);
-        JLabel label = new JLabel("Hello, world!");
-        label.setForeground(Color.GREEN);
-        label.setFont(new Font("Comic Sans", Font.PLAIN, 20));
-        label.setBackground(Color.BLACK);
-        label.setOpaque(true);
-        label.setBorder(border);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
-//        frame.setLayout(null);
-//        label.setBounds(0, 0, 350, 350);
-        frame.add(label);
-        frame.pack();
+        
     }
 }
