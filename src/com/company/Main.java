3ae01edfb87919aaa46c5b94e3ff7cacaeca5d1b
Luqman <luqdek@gmail.com>
2021-08-30 14:34:39 +0100

    create new panel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,9 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        JPanel panel = new JPanel();
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
