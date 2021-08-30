abb478731867eafe174be449395a01624506421e
Luqman <luqdek@gmail.com>
2021-08-30 14:33:23 +0100

    create basic frame

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,10 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(750,750);
+        frame.setVisible(true);
     }
 }
